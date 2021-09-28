e850eedaa48e1ca990195d69420a8f7253ce7765
C <chrismatusemail@gmail.com>
2021-09-28 22:50:41 +0100

    Parametrize unit test.

--- test_main.py
+++ test_main.py
@@ -1,47 +1,29 @@
-from unittest import TestCase
+import pytest
 from main import PolishNounHTMLParser
 from scraper_utils.common import scrape_word_data
 
 
-class TestPolishNounHTMLParser(TestCase):
+@pytest.mark.parametrize("input_words,expected_path,use_local_data", [
+    (["baba", "bałagan", "cel", "drzwi", "dzień", "małpa", "miesiąc", "rok", "ser"], "expected_output_polish_1", True),
+    (["prysznic", "glista", "gleba", "łeb", "palec", "noga", "piła", "piłka"], "expected_output_polish_2", False)
+])
+def test_PolishNounHTMLParser(input_words, expected_path, use_local_data):
+    output_path = f"output_test{expected_path[-2:]}"
 
-    def test_scraper_locally(self):
-        with open(f'output/expected_output_polish_1.json', 'r') as f:
-            expected = f.read()
-            f.close()
+    with open(f'output/{expected_path}.json', 'r') as f:
+        expected = f.read()
+        f.close()
 
-        scrape_word_data(
-            "Polish",
-            PolishNounHTMLParser(convert_charrefs=False),
-            ["baba", "bałagan", "cel", "drzwi", "dzień", "małpa", "miesiąc", "rok", "ser"],
-            True,
-            # ["prysznic", "glista", "gleba", "łeb", "palec", "noga", "piła", "piłka"],
-            # False,
-            "output_polish_1"
-        )
+    scrape_word_data(
+        "Polish",
+        PolishNounHTMLParser(convert_charrefs=False),
+        input_words,
+        use_local_data,
+        output_path
+    )
 
-        with open(f'output/output_polish_1.json', 'r') as f:
-            actual = f.read()
-            f.close()
+    with open(f'output/{output_path}.json', 'r') as f:
+        actual = f.read()
+        f.close()
 
-        assert actual == expected
-
-    def test_scraper_remotely(self):
-        with open(f'output/expected_output_polish_2.json', 'r') as f:
-            expected = f.read()
-            f.close()
-
-        scrape_word_data(
-            "Polish",
-            PolishNounHTMLParser(convert_charrefs=False),
-            ["prysznic", "glista", "gleba", "łeb", "palec", "noga", "piła", "piłka"],
-            False,
-            "output_polish_2"
-        )
-
-        with open(f'output/output_polish_2.json', 'r') as f:
-            actual = f.read()
-            f.close()
-
-        assert actual == expected
-
+    assert actual == expected
