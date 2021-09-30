fc650e8131ea84eecd3b18c473db9666cc82d1e1
C <chrismatusemail@gmail.com>
2021-09-30 23:56:20 +0100

    Standardise naming.

--- test_main.py
+++ test_main.py
@@ -3,12 +3,12 @@ from parser_classes.Polish_parsers import PolishNounHTMLParser
 from scraper_utils.common import scrape_word_data
 
 
-@pytest.mark.parametrize("input_words,expected_path,use_local_data", [
+@pytest.mark.parametrize("input_words,expected_path,use_sample", [
     (["baba", "bałagan", "cel", "drzwi", "dzień", "małpa", "miesiąc", "rok", "ser"], "polish_nouns_1", True),
     # (["prysznic", "glista", "gleba", "łeb", "BADWORD", "palec", "noga", "piła", "piłka"], "polish_nouns_2", False),
     (["nadzieja", "słońce", "wieczór", "sierpień", "ból", "złodziej", "wartość", "owca", "suszarka", "schody"], "polish_nouns_3", True),
 ])
-def test_PolishNounHTMLParser(input_words, expected_path, use_local_data):
+def test_PolishNounHTMLParser(input_words, expected_path, use_sample):
     output_path = f"output_test{expected_path[-2:]}"
 
     with open(f'expected/{expected_path}.json', 'r') as f:
@@ -19,7 +19,7 @@ def test_PolishNounHTMLParser(input_words, expected_path, use_local_data):
         "Polish",
         PolishNounHTMLParser(convert_charrefs=False),
         input_words,
-        use_local_data,
+        use_sample,
         output_path
     )
 
