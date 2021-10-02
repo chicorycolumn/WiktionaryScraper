6a46826d23a0cac08cf68714170fb0d5fc829777
C <chrismatusemail@gmail.com>
2021-10-02 14:11:41 +0100

    Tidy test.

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ if __name__ == '__main__':
     num = 1
     input_words = nouns_1[0:2]
 
+    print(f'# Starting, given {len(input_words)} words.')
+
     num = str(num)[-2:].zfill(2)
     output_path = f"output_nouns_{num}"
     rejected_path = f"rejected_nouns_{num}"
-
-    print(output_path, rejected_path)
 
     scrape_word_data(
         "Polish",
@@ -20,3 +20,5 @@ if __name__ == '__main__':
         output_file=output_path,
         rejected_file=rejected_path
     )
+
+    print(f"\n# Finished scraping for {len(input_words)} words:", input_words)
