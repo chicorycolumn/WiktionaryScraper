b826db19f63bcb1733585391a70754f7dd8b66a6
C <chrismatusemail@gmail.com>
2021-10-09 21:21:50 +0100

    Move instructions, edit shorthand.

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 from parser_classes.Polish_parsers import PolishNounHTMLParser
-from scraper_utils.processing import scrape_word_data
+from scraper_utils.processing import *
 from input.Polish.input_words import nouns_1
 
+""""
+Step One: Run scrape_word_data.
+    output_nouns_99 CREATED
+    truncated_nouns_99 CREATED
+    rejected_nouns_99 CREATED
+
+Step Two: Manually add shorthand tags
+    truncated_nouns_99 MODIFIED
+
+Step Three: Run untruncate_lemma_objects([99]).
+    untruncated_nouns_99 CREATED
+
+Step Four: Run fill_out_lemma_objects([99], wordtype).
+    finished_nouns_99 CREATED
+"""
+
 if __name__ == '__main__':
+
     # Group 1 = words 00 -  50
     # Group 2 = words 50 - 100
-
-    group_of_input_words = 2
-    input_words = nouns_1[50:100]  # After adding words input folder, import, then select here how many want scrape.
-    use_sample = False
-
-    print(f'## Starting, given {len(input_words)} words.')
-
     scrape_word_data(
-        PolishNounHTMLParser(convert_charrefs=False),
-        "Polish",
-        input_words,
-        use_sample=use_sample,
-        filepaths={
-            "output": f"output_nouns_{group_of_input_words}",
-            "rejected": f"rejected_nouns_{group_of_input_words}",
-            "truncated": f"truncated_nouns_{group_of_input_words}",
-        },
-        group_number=group_of_input_words
+        group_number=2,
+        head_words=nouns_1[50:100],  # After adding words input folder, import, then select here how many want scrape.
+        # # # # # # # # # #
+        parser=PolishNounHTMLParser(convert_charrefs=False),
+        language="Polish",
+        use_sample=False,
     )
 
-    print(f'\n## Scraped at "{f"output_nouns_{group_of_input_words}"}" for {len(input_words)} words:', input_words)
+    # untruncate_lemma_objects([1, 2])
+
+    # fill_out_lemma_objects([1], "noun")
+
