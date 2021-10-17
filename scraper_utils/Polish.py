d3821463a00e836e8bec7b8848ff5deb9a85f5f5
C <chrismatusemail@gmail.com>
2021-10-17 16:32:54 +0100

    Beginning to parse full inflections into minimised inflections.

--- scraper_utils/Polish.py
+++ scraper_utils/Polish.py
@@ -2,7 +2,7 @@
 def make_inflection_skeleton(lemma_object):
     full_inflections = lemma_object["inflections"]
 
-    res = {"verbal": {}}
+    res = {"verbal": {"past": {}, "present": {}, "future": {}, "conditional": {}, "imperative": {} }}
 
     def get_value_same_for_all_keys(key, final_key = None):
 
@@ -20,22 +20,82 @@ def make_inflection_skeleton(lemma_object):
         if not all(w == arr[0] for w in arr):
             raise Exception("#ERR Infinitives not all the same.")
 
-        res[final_key] = arr[0]
+        res["inflections"][final_key] = arr[0]
 
     get_value_same_for_all_keys("infinitive")
-    get_value_same_for_all_keys("passiveAdjectival", "passiveAdjectival")
     get_value_same_for_all_keys("verbalNoun", "verbalNoun")
 
     if lemma_object["aspect"] == "imperfective":
-        get_value_same_for_all_keys("activeAdjectival", "activeAdjectival")
-        get_value_same_for_all_keys("anteriorAdverbial", "anteriorAdverbial")
         get_value_same_for_all_keys("contemporaryAdverbial", "contemporaryAdverbial")
     elif lemma_object["aspect"] == "perfective":
-        get_value_same_for_all_keys("activeAdjectival", "activeAdjectival")
         get_value_same_for_all_keys("anteriorAdverbial", "anteriorAdverbial")
-        get_value_same_for_all_keys("contemporaryAdverbial", "contemporaryAdverbial")
     else:
         raise ValueError("#ERR Is neither perfective nor imperfective.")
+
+    # # # # # # #
+
+    list = [
+        full_inflections["past tense"]["impersonal"]["singular"]["masculine"],
+        full_inflections["past tense"]["impersonal"]["singular"]["feminine"],
+        full_inflections["past tense"]["impersonal"]["singular"]["neuter"],
+    ]
+
+    if not all(el == list[0] for el in list):
+        print("#ERR 441")
+
+    singular = list[0]
+
+    list = [
+        full_inflections["past tense"]["impersonal"]["plural"]["virile"],
+        full_inflections["past tense"]["impersonal"]["plural"]["nonvirile"],
+    ]
+
+    if not all(el == list[0] for el in list):
+        print("#ERR 442")
+
+    plural = list[0]
+
+    res["past"]["impersonal"] = {
+        "singular": {
+            "allSingularGenders": singular
+        },
+        "plural": {
+            "allPluralGenders": plural
+        }
+    }
+    # # # # # # #
+
+    list = [
+        full_inflections["past tense"]["impersonal"]["singular"]["masculine"],
+        full_inflections["past tense"]["impersonal"]["singular"]["feminine"],
+        full_inflections["past tense"]["impersonal"]["singular"]["neuter"],
+    ]
+
+    if not all(el == list[0] for el in list):
+        print("#ERR 441")
+
+    singular = list[0]
+
+    list = [
+        full_inflections["past tense"]["impersonal"]["plural"]["virile"],
+        full_inflections["past tense"]["impersonal"]["plural"]["nonvirile"],
+    ]
+
+    if not all(el == list[0] for el in list):
+        print("#ERR 442")
+
+    plural = list[0]
+
+    res["past"]["impersonal"] = {
+        "singular": {
+            "allSingularGenders": singular
+        },
+        "plural": {
+            "allPluralGenders": plural
+        }
+    }
+
+
 
 
 aspect_ref = {
