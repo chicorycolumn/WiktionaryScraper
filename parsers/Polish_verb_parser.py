7fb875ba6d0268f36f5bbad393e5ba100b163af0
C <chrismatusemail@gmail.com>
2021-11-27 14:59:24 +0000

    Polish Verb Parser can now handle a verb with two distinct meaning groups (with different otherShapes) but a single conj table. These are referred to as "diff_word_same_conj" and are marked as allohoms.

--- parsers/Polish_verb_parser.py
+++ parsers/Polish_verb_parser.py
@@ -1,3 +1,4 @@
+import copy
 from html.parser import HTMLParser
 from utils.scraping.Polish_dicts import aspect_ref
 from utils.scraping.common import orth, superstrip, add_string, trim_chaff_from_derived_terms, add_value_at_keychain
@@ -34,11 +35,13 @@ class PolishVerbParser(HTMLParser):
     current_cell_colspan = 1
     current_row_data = []
     current_derived_term = []
+    diff_word_same_conj_count = 0
+    diff_word_same_conj_objects = []
 
     got_derived_terms = False
     got_related_terms = False
 
-    def add_lobj_and_reset(self):
+    def add_lobj_and_reset(self, diff_word_same_conj: bool = False):
         t = [row["data"] for row in self.ingested_table]
         inflections = {}
         index_of_first_data_row = 0
@@ -79,8 +82,30 @@ class PolishVerbParser(HTMLParser):
         else:
             self.output_obj["aspect"] = aspect_ref[self.output_obj["aspect"][0]]
 
+        if diff_word_same_conj:
+            self.output_obj["diff_word_same_conj"] = True
+        else:
+            for out_obj in self.output_arr:
+                if "diff_word_same_conj" in out_obj and out_obj["diff_word_same_conj"]:
+                    out_obj["derivedTerms"] = copy.deepcopy(self.output_obj["derivedTerms"])
+                    out_obj["inflections"] = copy.deepcopy(self.output_obj["inflections"])
+                    out_obj["allohomInfo"] = None
+                    self.output_obj["allohomInfo"] = None
+                    out_obj.pop("diff_word_same_conj")
+
         self.output_arr.append(self.output_obj)
         self.reset_for_new_table()
+
+    def generate_empty_output_object(self):
+        return {
+            "lemma": None,
+            "aspect": [],
+            "secondaryAspects": [],
+            "otherShapes": {},
+            "tags": "xxxxxxxxx",
+            "translations": {"ENG": []},
+            "derivedTerms": []
+        }
 
     def add_new_row_obj(self):
         self.ingested_table.append({
@@ -97,17 +122,10 @@ class PolishVerbParser(HTMLParser):
         self.reset_for_new_cell()
 
         self.mode = None
+        print('mode = None')
         self.el_count = 0
         self.inflections = {}
-        self.output_obj = {
-            "lemma": None,
-            "aspect": [],
-            "secondaryAspects": [],
-            "otherShapes": {},
-            "tags": "xxxxxxxxx",
-            "translations": {"ENG": []},
-            "derivedTerms": []
-        }
+        self.output_obj = self.generate_empty_output_object()
         self.keys = []
         self.subkey = None
         self.current_definition = None
@@ -119,7 +137,8 @@ class PolishVerbParser(HTMLParser):
         self.col_num = 0
         self.ingested_table = []
         self.current_derived_term = []
-
+        self.diff_word_same_conj_count = 0
+        self.diff_word_same_conj_objects = []
 
     def reset_for_new_word(self):
         self.got_derived_terms = False
@@ -128,6 +147,7 @@ class PolishVerbParser(HTMLParser):
         self.output_arr = []
         self.keys = []
         self.location = None
+        print('location = None')
 
     def handle_data(self, data):
         data = orth(data)
@@ -153,8 +173,10 @@ class PolishVerbParser(HTMLParser):
                 if self.lasttag in ["h1", "h2", "h3", "h4", "h5"] or self.penultimatetag in ["h1", "h2", "h3", "h4", "h5"]:
                     if data.lower() in ["derived terms", "related terms"]:
                         self.mode = "gettingderivedterms"
+                        print('mode = "gettingderivedterms"')
                     else:
                         self.mode = "getderivedterms"
+                        print('mode = "getderivedterms"')
 
             if self.mode == "gettingdefinition":
                 self.current_definition = add_string(self.current_definition, data)
@@ -176,7 +198,13 @@ class PolishVerbParser(HTMLParser):
                     self.output_obj["lemma"] = data
 
             if self.penultimatetag in ["h1", "h2", "h3", "h4", "h5"] and self.lasttag == "span" and data.lower() == "verb":
+                self.diff_word_same_conj_count += 1
+
+                if self.diff_word_same_conj_count > 1:
+                    self.add_lobj_and_reset(diff_word_same_conj = True)
+
                 self.mode = "getaspect"
+                print('mode = "getaspect"')
 
         if self.location == "insidetable":
             self.current_row_data.append(data)
@@ -185,10 +213,13 @@ class PolishVerbParser(HTMLParser):
                 lang_in_focus = superstrip(data).lower()
                 if lang_in_focus == self.selected_lang:
                     self.location = "insideselectedlang"
+                    print('location = "insideselectedlang"')
                 else:
                     if self.location == "insideselectedlang":
                         self.mode = "END"
+                        print('mode = "END"')
                     self.location = None
+                    print('location = None')
 
 
     def handle_starttag(self, startTag, attrs):
@@ -220,10 +251,13 @@ class PolishVerbParser(HTMLParser):
             if self.mode == "gettable":
                 if startTag == "table" and self.currentclass and "inflection-table" in self.currentclass.split(" "):
                     self.mode = None
+                    print('mode = None')
                     self.location = "insidetable"
+                    print('location = "insidetable"')
 
             if self.mode == "getdefinitions" and startTag == "li":
                 self.mode = "gettingdefinition"
+                print('mode = "gettingdefinition"')
 
             if self.mode == "getaspect":
                 if startTag in ["i", "ol"]:
@@ -235,6 +269,7 @@ class PolishVerbParser(HTMLParser):
                         self.output_obj["secondaryAspects"].append(self.current_otherShapes_key)
                 if startTag == "ol":
                     self.mode = "getdefinitions"
+                    print('mode = "getdefinitions"')
 
     def handle_endtag(self, endTag):
         if self.mode and (self.mode == "END" or endTag in ["body", "html"]):
@@ -242,7 +277,9 @@ class PolishVerbParser(HTMLParser):
             self.add_lobj_and_reset()
 
             self.location = None
+            print('location = None')
             self.mode = None
+            print('mode = None')
             aalobj = self.output_obj
             aaouta = self.output_arr
             return
@@ -250,10 +287,13 @@ class PolishVerbParser(HTMLParser):
         if self.location == "insidetable":
             if endTag == "table":
                 self.location = "insideselectedlang"
+                print('location = "insideselectedlang"')
                 self.mode = "getderivedterms"
+                print('mode = "getderivedterms"')
 
             elif endTag == "tr":
                 self.mode = None
+                print('mode = None')
                 self.ingested_table[self.row_num]["closed"] = True
                 self.row_num += 1
 
@@ -295,9 +335,11 @@ class PolishVerbParser(HTMLParser):
             self.output_obj["translations"]["ENG"].append(self.current_definition)
             self.current_definition = None
             self.mode = "getdefinitions"
+            print('mode = "getdefinitions"')
 
         if endTag == "ol" and self.mode == "getdefinitions":
             self.mode = "gettable"
+            print('mode = "gettable"')
 
         self.lsEndTags.append(endTag)
         self.lsAll.append(endTag)
