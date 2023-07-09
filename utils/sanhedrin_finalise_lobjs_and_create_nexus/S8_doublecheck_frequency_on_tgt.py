6b840f60b02407b490fca94d49daf73907cdcd97
C <chrismatusemail@gmail.com>
2023-07-09 16:08:39 +0100

    Update freq on target lobjs.

--- utils/sanhedrin_finalise_lobjs_and_create_nexus/S8_doublecheck_frequency_on_tgt.py
+++ utils/sanhedrin_finalise_lobjs_and_create_nexus/S8_doublecheck_frequency_on_tgt.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     # # # # # #
     wordtype = "adj"
     batch = "01"
-    step = 1  # Step 1 to record changes. Step 2 to apply changes.
+    step = 2  # Step 1 to record changes. Step 2 to apply changes.
     # # # # # #
 
     input_filename = f"{wordtype}_batch_{batch}_TGT"
@@ -68,61 +68,59 @@ if __name__ == '__main__':
             c.print_teal("Loaded " + str(len(to_change_lobjs)) + " items from tempsave.")
             f.close()
 
-    sorted_lobjs["F1"] = [l for l in lobjs if int(l["frequency"]) == 1]
-    sorted_lobjs["F2"] = [l for l in lobjs if int(l["frequency"]) == 2]
-    sorted_lobjs["F3"] = [l for l in lobjs if int(l["frequency"]) == 3]
-    sorted_lobjs["F4"] = [l for l in lobjs if int(l["frequency"]) == 4]
-    sorted_lobjs["F5"] = [l for l in lobjs if int(l["frequency"]) == 5]
+    if step == 1:
+        sorted_lobjs["F1"] = [l for l in lobjs if int(l["frequency"]) == 1]
+        sorted_lobjs["F2"] = [l for l in lobjs if int(l["frequency"]) == 2]
+        sorted_lobjs["F3"] = [l for l in lobjs if int(l["frequency"]) == 3]
+        sorted_lobjs["F4"] = [l for l in lobjs if int(l["frequency"]) == 4]
+        sorted_lobjs["F5"] = [l for l in lobjs if int(l["frequency"]) == 5]
 
-    print("")
-    for num in [1,2,3,4,5]:
         print("")
-        print("")
-        print(len(sorted_lobjs["F" + str(num)]), f"lobjs with frequency category {str(num)}.")
+        for num in [1,2,3,4,5]:
+            print("")
+            print("")
+            print(len(sorted_lobjs["F" + str(num)]), f"lobjs with frequency category {str(num)}.")
 
-        temp_arr = []
+            temp_arr = []
 
-        for lebindex, leb in enumerate(sorted_lobjs["F" + str(num)]):
-            if to_change_lobjs[f"F{str(num)} done up to"] > 0 and lebindex <= to_change_lobjs[f"F{str(num)} done up to"]:
-                print("Already done", lebindex)
-                continue
+            for lebindex, leb in enumerate(sorted_lobjs["F" + str(num)]):
+                if to_change_lobjs[f"F{str(num)} done up to"] > 0 and lebindex <= to_change_lobjs[f"F{str(num)} done up to"]:
+                    print("Already done", lebindex)
+                    continue
 
-            if lebindex != 0 and ((lebindex + 1 == len(sorted_lobjs["F" + str(num)])) or (lebindex % 20 == 0)):
-                temp_arr.append([lebindex, leb])
+                if lebindex != 0 and ((lebindex + 1 == len(sorted_lobjs["F" + str(num)])) or (lebindex % 20 == 0)):
+                    temp_arr.append([lebindex, leb])
 
-                c.print_purple(f'Are these frequency category {str(num)}?')
-                for ho in temp_arr:
-                    printable_lebindex = f"{' ' if ho[0] < 10 else ''}{ho[0]}"
-                    print(c.bold(printable_lebindex), c.blue(ho[1]["id"]), ho[1]["»trans"])
-                c.print_purple(f'Are these frequency category {str(num)}?')
-                print("")
+                    c.print_purple(f'Are these frequency category {str(num)}?')
+                    for ho in temp_arr:
+                        printable_lebindex = f"{' ' if ho[0] < 10 else ''}{ho[0]}"
+                        print(c.bold(printable_lebindex), c.blue(ho[1]["id"]), ho[1]["»trans"])
+                    c.print_purple(f'Are these frequency category {str(num)}?')
+                    print("")
 
-                requested_changes = get_new_freqs(temp_arr)
+                    requested_changes = get_new_freqs(temp_arr)
 
-                for requested_change in requested_changes:
-                    new_freq = requested_change[0]
-                    id_of_lobj_to_change = requested_change[1]
-                    to_change_lobjs[f"F{str(new_freq)}"].append(id_of_lobj_to_change)
+                    for requested_change in requested_changes:
+                        new_freq = requested_change[0]
+                        id_of_lobj_to_change = requested_change[1]
+                        to_change_lobjs[f"F{str(new_freq)}"].append(id_of_lobj_to_change)
 
-                to_change_lobjs[f"F{str(num)} done up to"] = lebindex
-                save(to_change_lobjs, True)
-                temp_arr = []
-            else:
-                temp_arr.append([lebindex, leb])
+                    to_change_lobjs[f"F{str(num)} done up to"] = lebindex
+                    save(to_change_lobjs, True)
+                    temp_arr = []
+                else:
+                    temp_arr.append([lebindex, leb])
 
-        save(to_change_lobjs, True)
+            save(to_change_lobjs, True)
+    elif step == 2:
+        for num in [1,2,3,4,5]:
+            ids_of_lobjs_to_change = to_change_lobjs[f"F{str(num)}"]
+            print(len(ids_of_lobjs_to_change), "lobjs to change to frequency", c.bold(str(num)))
 
-    # user_input = input("Which frequency category do you want to check? 1-5 ")
-    #
-    # if user_input and user_input in "12345":
-    #     freq_to_check = user_input
-    #     lobjs_to_check = sorted_lobjs["F" + str(freq_to_check)]
-    #     for lindex, lob in enumerate(lobjs_to_check):
-    #         print(f"{lindex+1}/{len(lobjs_to_check)}")
-    #         print(f'{c.purple(lob["lemma"])} {c.blue(lob["id"])}')
-    #         user_input = get_freq(lob, f"{freq_to_check} OK?   Enter for yes   Or enter new frequency ", True)
-    #         if user_input:
-    #             lob["frequency"] = int(user_input)
-    #
-    print("Completely done.")
-    save(lobjs)
+            for ids_of_lobj_to_change in ids_of_lobjs_to_change:
+                for lobj in lobjs:
+                    if lobj["id"] == ids_of_lobj_to_change:
+                       lobj["frequency"] = num
+
+        print("Completely done.")
+        save(lobjs)
