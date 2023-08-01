9df42edf259a19900674cfe16f024bda890bd5dc
C <chrismatusemail@gmail.com>
2023-08-01 18:57:03 +0100

    Reorder keys on lobjs.

--- utils/sanhedrin_finalise_lobjs_and_create_nexus/S11_reorder_keys_of_lobjs.py
+++ utils/sanhedrin_finalise_lobjs_and_create_nexus/S11_reorder_keys_of_lobjs.py
@@ -1,6 +1,7 @@
 from parsers.common import scrape_word_data
 from utils.general.common import write_todo
-from utils.sanhedrin_finalise_lobjs_and_create_nexus.tools import is_it_the_same_meaning, q, add_signalwords, get_signalword, test_signalword
+from utils.sanhedrin_finalise_lobjs_and_create_nexus.tools import is_it_the_same_meaning, q, add_signalwords, \
+    get_signalword, test_signalword
 from utils.postprocessing.common import finalise_lemma_objects
 from utils.scraping.common import check_rescraped_against_existing
 from utils.universal import color as c, get_curried_save, load_tempsave_if_exists, load_data, deepequals
@@ -10,13 +11,34 @@ if __name__ == '__main__':
     # # # # # #
     batch = "01"
     suffix = "SRC"
-    wordtype = "nco"
+    wordtype = "ver"
     # # # # # #
 
     inflection_keys_ref = {
         "adj": ["adverb", "simple", "comparative", "superlative"],
         "nco": ["singular", "plural"],
         "npe": ["singular", "plural"],
+        "ver": [
+            "infinitive",
+            "verbalNoun",
+            "activeAdjectival",
+            "passiveAdjectival",
+            "contemporaryAdverbial",
+            "anteriorAdverbial",
+            "verbal"
+        ],
+        "verbal": [
+            "conditional",
+            "future",
+            "imperative",
+            "past",
+            "present",
+        ]
+    }
+
+    lobj_keys_ref = {
+        "ver": ["lemma", "id", "aspect", "secondaryAspects", "tags", "topics", "frequency", "register", "allohomInfo",
+                "inflections", "translations", "extra", "derivedTerms", "otherShapes"]
     }
 
     inflection_keys = inflection_keys_ref[wordtype]
@@ -47,9 +69,43 @@ if __name__ == '__main__':
 
         lobj["inflections"] = reordered_inflections
 
+        if wordtype == "ver":
+            reordered_verbal_inflections = {}
+
+            for k in lobj["inflections"]["verbal"]:
+                if k not in inflection_keys_ref["verbal"]:
+                    c.print_red(f'Did not expect inflection key "{k}"')
+                    raise Exception("Stop")
+
+            for k in inflection_keys_ref["verbal"]:
+                if k in lobj["inflections"]["verbal"]:
+                    reordered_verbal_inflections[k] = lobj["inflections"]["verbal"][k]
+
+            lobj["inflections"]["verbal"] = reordered_verbal_inflections
+
         done_lobjs.append(lobj)
 
-    save(done_lobjs)
+    if wordtype in lobj_keys_ref:
+        double_done_lobjs = []
+
+        inflection_keys = lobj_keys_ref[wordtype]
+        for lindex, lobj in enumerate(done_lobjs):
+            reordered_lobj = {}
+
+            for k in lobj:
+                if k not in inflection_keys:
+                    c.print_red(f'Did not expect lobj key "{k}"')
+                    raise Exception("Stop")
+
+            for k in inflection_keys:
+                if k in lobj:
+                    reordered_lobj[k] = lobj[k]
+
+            double_done_lobjs.append(reordered_lobj)
+
+        save(double_done_lobjs)
+    else:
+        save(done_lobjs)
 
     print("")
     print("Completely done.")
