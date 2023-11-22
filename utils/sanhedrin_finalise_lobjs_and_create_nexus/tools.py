0f7333567613d2ceb7b18841e37a34fc410ae239
C <chrismatusemail@gmail.com>
2023-11-22 09:19:52 +0000

    Improve S17 for nco.

--- utils/sanhedrin_finalise_lobjs_and_create_nexus/tools.py
+++ utils/sanhedrin_finalise_lobjs_and_create_nexus/tools.py
@@ -956,32 +956,19 @@ def get_inflections_eng_ver(lemma, manually_entered_inflections: [str] = None, r
 
 def get_inflections_eng_nou(
         lemma,
-        manually_entered_inflection: str = None,
+        manually_entered_inflections: [str] = None,
         reconfirming: bool = False,
-        remove_sas: bool = False,
-        flip_sas: bool = False,
-        plural_is_same: bool = False,
+        keep_terminal_y: bool = False,
 ):
     def restart():
         return get_inflections_eng_nou(lemma)
 
-    if manually_entered_inflection:
-        plur_nom = manually_entered_inflection
-        sing_gen = lemma + "'s"
-        plur_gen = plur_nom + "'s"
-        if remove_sas:
-            plur_gen = plur_gen[0:-2]
-            plur_gen = plur_gen + "'"
-        if flip_sas:
-            plur_gen = replace_char_at_index(plur_gen, -2, '')
-            plur_gen = plur_gen + "'"
-        if plural_is_same:
-            plur_gen = sing_gen
-            plur_nom = lemma
+    if manually_entered_inflections:
+        plur_nom, sing_gen, plur_gen = manually_entered_inflections
     else:
         sing_gen = lemma + "'s"
-        plur_nom = lemma + ("s" if (lemma[-1] not in 'hxsy' or lemma.endswith('th')) else "es")
-        if lemma.endswith('y'):
+        plur_nom = lemma + ("s" if (keep_terminal_y or lemma[-1] not in 'hxsy' or lemma.endswith('th')) else "es")
+        if lemma.endswith('y') and not keep_terminal_y:
             plur_nom = replace_char_at_index(plur_nom, -3, 'i')
         plur_gen = plur_nom + "'"
 
@@ -990,7 +977,7 @@ def get_inflections_eng_nou(
     print_function(f'{lemma}, {plur_nom}')
     print_function(f'{sing_gen}, {plur_gen}')
     print("")
-    user_input = input('Enter YES\nAny   NO\nq     Remove "s`s"\nw     Flip "`s"\nq     Plural is same as singular\nOr type in manually nominative plural eg "men" and press enter\n: ')
+    user_input = input('Enter for YES / Any for NO\nq     Keep terminal y\nw     Plural same as singular\nOr type in irregular plural eg "men" and press Enter\n: ')
 
     if not user_input or user_input == 'y':
         return {
@@ -1005,11 +992,13 @@ def get_inflections_eng_nou(
         }
 
     if user_input == 'q':
-        return get_inflections_eng_nou(lemma, plur_nom, True, True)
+        return get_inflections_eng_nou(lemma, None, True, True)
+
     if user_input == 'w':
-        return get_inflections_eng_nou(lemma, plur_nom, True, False, True)
-    if user_input == 'e':
-        return get_inflections_eng_nou(lemma, plur_nom, True, False, False, True)
+        plur_nom = lemma
+        sing_gen = lemma + "'s"
+        plur_gen = sing_gen
+        return get_inflections_eng_nou(lemma, [plur_nom, sing_gen, plur_gen], True)
 
     if len(user_input) == 1:
         return restart()
@@ -1018,7 +1007,16 @@ def get_inflections_eng_nou(
         if len(user_input) < 2 or " " in user_input:
             c.print_red("You must type one string: nominative plural")
             return restart()
-        return get_inflections_eng_nou(lemma, user_input, True)
+
+        plur_nom = user_input.split(' ')[1]
+        sing_gen = lemma + "'s"
+
+        if plur_nom.endswith('s'):
+            plur_gen = sing_gen + "'"
+        else:
+            plur_gen = sing_gen + "'s"
+
+        return get_inflections_eng_nou(lemma, [plur_nom, sing_gen, plur_gen], True)
 
 
 def get_inflections_eng_adj(lemma, manually_entered_inflections: [str] = None, reconfirming: bool = False):
